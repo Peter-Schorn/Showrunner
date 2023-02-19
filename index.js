1d6356556661be0e2f43f7ab34aec95ff1410236
aeridy <awilliams@mvbalaw.com>
2023-02-18 18:25:46 -0600

    added queries.js to store code snippets temporarily

--- index.js
+++ index.js
@@ -79,64 +79,7 @@ app.get('/home', (req, res) => {
 app.get('/error', (req, res) =>
 res.render('error.ejs'))
 
-// CREATE USER
 
-// Blueprint
-
-// 1 - Schema
-
-const userSchema = mongoose.Schema({
-    emailAddress: {
-        type: String,
-        minLength: 6,
-        required: [true, "no data provided"]
-    },
-    password: {
-        type: String,
-        minLength: 4,
-        required: [true, "no data provided"]
-    },
-    firstName: {
-        type: String,
-        minLength: 2,
-        required: [true, "no data provided"]
-    },
-    lastName: {
-        type: String,
-        minLength: 2,
-        required: [true, "no data provided"]
-    },
-    created: {
-        type: Date, 
-        default: Date.now()
-    }
-})
-
-// 2 - Model
-
-const UserModel = new mongoose.model('users', userSchema)
-
-// Queries
-
-// // Create user data
-
-// let newUser = new UserModel({
-//     emailAddress: '',
-//     password: '',
-//     firstName: '',
-//     lastName: ''
-// })
-
-// // Save user to the database
-
-// newUser.save((err, data)=>{
-//     if(err){
-//         console.log(`There was an error saving new user data to the db: `,err.message)
-//     } else {
-//         console.log(`New User was saved!`);
-//         console.log(data);
-//     }
-// })
 
 
 
