000728e08a600f74f0883f12692b346db44a722d
Peter Schorn <petervschorn@gmail.com>
2023-03-11 01:25:13 -0600

    The full show objects returned by `userFullShows` now also contain a `userShow` property that contains the user show object.

--- models/updateShowModel.js
+++ models/updateShowModel.js
@@ -89,27 +89,39 @@ exports.userFullShows = function(userId) {
             
             const showIds = (user?.userShows ?? []).map((show) => `${show?.showId}`);
             
-            return ShowModel.find({ "showId": { $in: showIds } }).then((showObjects) => {
-                
-                const foundIds = showObjects.map(show => `${show?.showId}`);
-                const remainingIds = showIds.filter((id) => {
-                    return !foundIds.includes(id);
+            return ShowModel.find({ "showId": { $in: showIds } })
+                .lean()
+                .then((showObjects) => {
+                    
+                    for (let i = 0; i < showObjects.length; i++) {
+                        const show = showObjects[i];
+                        const userShow = user.userShows.find((userShow) => {
+                            return userShow.showId.toString() === show.showId.toString();
+                        });
+                        if (userShow) {
+                            showObjects[i].userShow = userShow;
+                        }
+                    }
+                        
+                    const foundIds = showObjects.map(show => `${show?.showId}`);
+                    const remainingIds = showIds.filter((id) => {
+                        return !foundIds.includes(id);
+                    });
+                    
+                    // console.log({showIds});
+                    // console.log({foundIds});
+                    // console.log({remainingIds});
+                    
+                    const addToDatabasePromises = remainingIds.map((id) => {
+                        return exports.addShowToDatabase(id);
+                    })
+                    console.log(
+                        `addToDatabasePromises.length: ${addToDatabasePromises.length}`
+                    );
+                    return Promise.all(addToDatabasePromises).then((shows) => {
+                        return showObjects.concat(shows);
+                    });
                 });
-                
-                // console.log({showIds});
-                // console.log({foundIds});
-                // console.log({remainingIds});
-                
-                const addToDatabasePromises = remainingIds.map((id) => {
-                    return exports.addShowToDatabase(id);
-                })
-                console.log(
-                    `addToDatabasePromises.length: ${addToDatabasePromises.length}`
-                );
-                return Promise.all(addToDatabasePromises).then((shows) => {
-                    return showObjects.concat(shows);
-                });
-            });
             
         });
         
